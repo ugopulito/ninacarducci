fede15beb55e468a4cf65e3fe786fb44e3aaee47
Ugo <pulito.ugo@gmail.com>
2023-04-02 15:40:46 +0200

    rollback to minify

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,19 @@
-document.addEventListener('DOMContentLoaded', () =>{
+$(document).ready(function() {
+    $('.gallery').mauGallery({
+        columns: {
+            xs: 1,
+            sm: 2,
+            md: 3,
+            lg: 3,
+            xl: 3
+        },
+        lightBox: true,
+        lightboxId: 'myAwesomeLightbox',
+        showTags: true,
+        tagsPosition: 'top'
+    });
     //Scroll to top
-    document.querySelector('.name').addEventListener('click', () => {
+    $('.name').on('click', () => {
         scrollTo(0,0);
     })
-})
+});
